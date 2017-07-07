1b29cfeda4239b2109d5f3a35f2b8429494e8a33
Chuang, Yun-Shiuan <vimchiz@gmail.com>
2017-07-08 00:08:05 +0800

    Add source code
    add source code

---
+++ label4MRI/R/mni_to_region_index.R
@@ -0,0 +1,29 @@
+
+mni_to_region_index<-function(x,y,z,distance=T)
+{
+
+  index=as.numeric(subset(mni2aal, x_mni==round(x) &
+                          y_mni==round(y) & z_mni==round(z),
+                          select=Region_index))
+
+  if(distance==T)
+    {
+      if(is.na(index)==F)
+      {return(list(index,distance=0))}
+      else
+      {d_x=(mni2aal$x_mni-x)^2
+      d_y=(mni2aal$y_mni-y)^2
+      d_z=(mni2aal$z_mni-z)^2
+      d2=colSums(rbind(d_x,d_y,d_z))
+      index=mni2aal$Region_index[which.min(d2)]
+      distance=sqrt(min(d2))
+      return(list(index,distance))}
+     }
+  else if (distance==F)
+    {
+      if(is.na(index)==F)
+      {return(list(index))}
+    else{(return(paste0("Not exactly correspond to aal-labeled brain region. Please set distance=T if youn want the nearest aal-labeled region name.")))}
+     }
+}
+
---
+++ label4MRI/R/mni_to_region_name.R
@@ -0,0 +1,30 @@
+#' @title MRI-labeling: MNI to AAL
+#' @description Input an MNI coordinate, output the corresponding AAL (Automated Anatomical Labeling) brain region name.
+#' @param x A number
+#' @param y A number
+#' @param z A number
+#' @param distance  A logical value which indicates whether the distance-to-nearest-aal-region information should be shown (default=T). This could be turned off to speed up labeling process.
+#' @export
+#' @seealso \code{\link[utils]{head}}
+#' @return If distance mode is on (distance=T), output a list with brain region name along with the corresponding distance.If distance=F, output a string of region name when available, otherwise output "Not exactly correspond to aal-labeled brain region. Please set distance=T if you want the nearest aal-labeled region name.".
+#' @examples
+#' # aal-corresponding point with distance mode on
+#' mni_to_region_name(26,0,0,distance=T)
+#' # aal-corresponding point with distance mode off (much faster)
+#' mni_to_region_name(26,0,0,distance=F)
+#'
+#' #non-aal-corresponding point with distance mode on (output the nearest aal region name)
+#' mni_to_region_name(0,0,0,distance=T)
+#' #non-aal-corresponding point with distance mode off (output none aal region name)
+#'mni_to_region_name(0,0,0,distance=F)
+mni_to_region_name=function(x,y,z,distance=T)
+{r_index=mni_to_region_index(x,y,z,distance=distance)
+if (distance==T)
+{
+  return(list(region=region_index_to_name(r_index[[1]]),
+              distance=r_index[[2]]))
+}
+else if (distance==F && !is.character(r_index))
+{return(region=region_index_to_name(r_index[[1]]))}
+else {return(region=r_index)}
+}
