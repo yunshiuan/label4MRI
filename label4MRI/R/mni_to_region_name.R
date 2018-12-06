b1d2e59800d704d6184c9e91beb7a4269865999d
yunshiuan <vimchiz@gmail.com>
2018-12-06 20:17:26 +0800

    Omit brain region index from result
    Omit brain region index from result. Only return region name and the corresponding distance. See #2.

--- label4MRI/R/mni_to_region_name.R
+++ label4MRI/R/mni_to_region_name.R
@@ -10,43 +10,48 @@
 #' @return If distance mode is on (distance=T), output a list with brain region name along with the corresponding distance.If distance=F, output a string of region name when available, otherwise output 'Not exactly correspond to aal-labeled brain region. Please set distance=T if you want the nearest aal-labeled region name.'.
 #' @examples
 #' # aal-corresponding point with distance mode on
-#' mni_to_region_name(26,0,0,distance=T)
+#' mni_to_region_name(26, 0, 0, distance = T)
 #' # aal-corresponding point with distance mode off (much faster)
-#' mni_to_region_name(26,0,0,distance=F)
+#' mni_to_region_name(26, 0, 0, distance = F)
 #'
 #' # non-aal-corresponding point with distance mode on (output the nearest aal region name)
-#' mni_to_region_name(0,0,0,distance=T)
+#' mni_to_region_name(0, 0, 0, distance = T)
 #' # non-aal-corresponding point with distance mode off (output none aal region name)
-#' mni_to_region_name(0,0,0,distance=F)
+#' mni_to_region_name(0, 0, 0, distance = F)
 #'
 #' # query both aal and ba structure
-#' mni_to_region_name(26,0,0,distance=T)
-mni_to_region_name <- function(x, y, z, distance = T, template = c('aal', 'ba')) {
+#' mni_to_region_name(26, 0, 0, distance = T)
+mni_to_region_name <- function(x, y, z, distance = T, template = c("aal", "ba")) {
   if_template_exist <- template %in% names(label4mri_metadata)
-  
-  if(sum(!if_template_exist) != 0) {
-    stop(paste0('Template `', paste(template[!if_template_exist], collapse = ', '), '` does not exist.'))
+
+  if (sum(!if_template_exist) != 0) {
+    stop(paste0("Template `", paste(template[!if_template_exist], collapse = ", "), "` does not exist."))
   }
-  
+
   r_indexes <- lapply(
     template,
     function(.template) {
       result <- mni_to_region_index(x, y, z, distance, .template)
-      
+      df_region_index_name <- label4mri_metadata[[.template]]$label
+
       result$label <- as.character(
-        label4mri_metadata[[.template]]$label[label4mri_metadata[[.template]]$label$Region_index == result$index, "Region_name"]
+        df_region_index_name[
+          df_region_index_name$Region_index == result$index,
+          "Region_name"
+        ]
       )
-      result$label <- ifelse(length(result$label)==0,"NULL",result$label)
+      result$label <- ifelse(length(result$label) == 0, "NULL", result$label)
+      result$index <- NULL
       result
     }
   )
-  
+
   result <- unlist(r_indexes, recursive = F)
   names(result) <- paste(
     rep(template, each = 3),
-    rep(c('index', 'distance', 'label'), length(template)),
-    sep = '.'
+    rep(c("index", "distance", "label"), length(template)),
+    sep = "."
   )
-  
+
   result
 }
