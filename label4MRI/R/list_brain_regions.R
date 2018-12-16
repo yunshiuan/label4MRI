84cf4adf46e0f90536870aefdff190379af5cd9c
yunshiuan <vimchiz@gmail.com>
2018-12-16 14:34:59 +0800

    Update list_brain_regions.R
    Get rid of the need of setNames()

--- label4MRI/R/list_brain_regions.R
+++ label4MRI/R/list_brain_regions.R
@@ -3,16 +3,20 @@
 #' @param template A character value which indicates the templates of interest
 #' (\code{"aal"} or \code{"ba"}). Use both of them by default.
 #' @return Return a list of charcter vectors.
-#'Each character vector is all the brain region names defined by one of the template.
+#' Each character vector is all the brain region names defined by one of the template.
 #' @examples
 #' # List all brain region names defined by AAL.
 #' list_brain_regions(template = "aal")
 #' @export
 
- list_brain_regions <- function(template = c("aal", "ba")) {
-    lapply(setNames(template,template),
-           function(.template){
-             as.character(label4mri_metadata[[.template]]$label$Region_name)
-           })
- }
-
+list_brain_regions <- function(template = c("aal", "ba")) {
+  list_of_brain_regions <-
+    lapply(
+      template,
+      function(.template) {
+        as.character(label4mri_metadata[[.template]]$label$Region_name)
+      }
+    )
+  names(list_of_brain_regions) <- template
+  list_of_brain_regions
+}
