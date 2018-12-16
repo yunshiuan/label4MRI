209f8c72c37f4c35c2a090168df2024cd0f0a707
yunshiuan <vimchiz@gmail.com>
2018-12-16 14:14:52 +0800

    Create list_brain_regions.R
    Add the function list_brain_regions()

---
+++ label4MRI/R/list_brain_regions.R
@@ -0,0 +1,18 @@
+#' @title List all brain region names in AAL/BA system.
+#' @description Input template names, output all the brain region names.
+#' @param template A character value which indicates the templates of interest
+#' (\code{"aal"} or \code{"ba"}). Use both of them by default.
+#' @return Return a list of charcter vectors.
+#'Each character vector is all the brain region names defined by one of the template.
+#' @examples
+#' # List all brain region names defined by AAL.
+#' list_brain_regions(template = "aal")
+#' @export
+
+ list_brain_regions <- function(template = c("aal", "ba")) {
+    lapply(setNames(template,template),
+           function(.template){
+             as.character(label4mri_metadata[[.template]]$label$Region_name)
+           })
+ }
+
