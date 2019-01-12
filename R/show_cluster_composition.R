8f9521edf1d1ef557cada9610cea8464cc6757cc
chichuanchen <46586524+chichuanchen@users.noreply.github.com>
2019-01-12 18:05:14 +0800

    CC's first attempt writing show_cluster_composition

--- R/show_cluster_composition.R
+++ R/show_cluster_composition.R
@@ -25,10 +25,43 @@
 #' @keywords internal
 #' @noRd
 
-show_cluster_composition=function(coordinate_matrix,template){
-        #Step1 : Call the function mni_to_region_name to compute the composition
-        ...
-        #Step2 : Output the result as a list
-        return(list(region_name=region_name,
-                    percentage = percentage))
+
+
+show_cluster_composition <- function(coordinate_matrix, template = c("aal", "ba")) {
+  ## Step1 : Call the function mni_to_region_name to compute the composition
+
+  # my logic & thought process:
+  # what form will coordinate_matrix take? now assumed: 3 by n matrix
+  # example matrix used: ccmatrix <- matrix(4:39, nrow = 3)
+  # read all the coordinates in the matrix, output a list of result
+  # ignore distance for now? maybe can output text warning if some distance != 0
+  # count the number of coordinates, set as denaminator
+  # count unique region names, set as numerator
+  # fraction * 100% = percentage
+  m <- coordinate_matrix
+  Result <- t(mapply(FUN = mni_to_region_name, x = m[1, ], y = m[2, ], z = m[3, ]))
+
+
+  if (template == "aal") {
+    # all_regions <- unlist(Result[, 2])
+    all_regions <- Result[, 2]
+  } else if (template == "ba") {
+    all_regions <- Result[, 4]
+  }
+
+  unique_comp <- unique(all_regions)
+
+  percentage <- lapply(unique_comp,
+    FUN = function(.unique_comp) {
+      whole_num <- ncol(m)
+      part_num <- sum(all_regions == .unique_comp)
+      round((part_num / whole_num) * 100, digits = 2)
+    }
+  )
+
+  # Step2 : Output the result as a list
+  return(list(
+    region_names = unique_comp,
+    percentage = percentage
+  ))
 }
