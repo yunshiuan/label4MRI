76dd4fbf36ee5019f999debdcd64be585e1d2e54
yunshiuan <vimchiz@gmail.com>
2019-01-17 14:25:22 +0800

    Revise the implementation of show_cluster_composition()
    Now support both single and multiple templates.

--- R/show_cluster_composition.R
+++ R/show_cluster_composition.R
@@ -25,29 +25,71 @@ show_cluster_composition <- function(coordinate_matrix, template = c("aal", "ba"
     stop(paste0("Template `", paste(template[!if_template_exist], collapse = ", "), "` does not exist."))
   }
 
-  result <- t(mapply(
-    FUN = mni_to_region_name,
-    x = coordinate_matrix[1, ],
-    y = coordinate_matrix[2, ],
-    z = coordinate_matrix[3, ],
-    distance = F,
-    MoreArgs = list(template = template)
-  ))
+  # result <- t(mapply(
+  #   FUN = mni_to_region_name,
+  #   x = coordinate_matrix[1, ],
+  #   y = coordinate_matrix[2, ],
+  #   z = coordinate_matrix[3, ],
+  #   distance = F,
+  #   MoreArgs = list(template = template)
+  # ))
 
-  all_regions <- apply(result[, paste0(template, ".label")], 2, as.list)
+  list_frequency_table <-
+    lapply(
+      template,
+      function(.template) {
+        # Label the coordinate matrix
+        labeled_coordinate_matrix <-
+          mapply(
+            FUN = mni_to_region_name,
+            x = coordinate_matrix[1, ],
+            y = coordinate_matrix[2, ],
+            z = coordinate_matrix[3, ],
+            distance = F,
+            MoreArgs = list(template = .template)
+          )
 
-  unique_regions <- lapply(all_regions,
-    FUN = unique
-  )
+        # Create a frequency table of the coordinates of each brain region
+        brain_frequency <-
+          table(unlist(labeled_coordinate_matrix[paste0(.template, ".label"), ]))
 
-  percentage <-
-    lapply(seq_along(all_regions),
-      FUN = function(tem_index) {
-        region_table <- table(unlist(all_regions[[tem_index]]))
+        sorted_brain_frequency <-
+          sort(brain_frequency, decreasing = T)
 
-        paste0(round((unname(region_table) / sum(unname(region_table))) * 100, digits = 2), "%")
+        # Add a column of percentate of each brain region
+        sorted_brain_percentage <-
+          round(sorted_brain_frequency / ncol(coordinate_matrix), 3) * 100
+
+        sorted_table_frequency_percentage <-
+          cbind(sorted_brain_frequency, sorted_brain_percentage)
+
+        colnames(sorted_table_frequency_percentage) <-
+          c("Number of coordinates", "Percentage (%)")
+
+        sorted_table_frequency_percentage
       }
     )
 
-  return(list(unique_regions, percentage))
+  names(list_frequency_table) <- paste(template,
+    "cluster",
+    "composition",
+    sep = "."
+  )
+
+  # all_regions <- apply(result[, paste0(template, ".label")], 2, as.list)
+  #
+  # unique_regions <- lapply(all_regions,
+  #   FUN = unique
+  # )
+
+  # percentage <-
+  #   lapply(seq_along(all_regions),
+  #     FUN = function(tem_index) {
+  #       region_table <- table(unlist(all_regions[[tem_index]]))
+  #
+  #       paste0(round((unname(region_table) / sum(unname(region_table))) * 100, digits = 2), "%")
+  #     }
+  #   )
+
+  list_frequency_table
 }
